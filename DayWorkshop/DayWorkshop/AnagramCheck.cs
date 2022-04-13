2ba5f4782cfa35de47b0803ef8ef21c58e683425
sanjay <ssah6694@gmail.com>
2022-04-13 08:55:05 +0530

    [sanjay]Added: Anagram file.

---
+++ DayWorkshop/DayWorkshop/AnagramCheck.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayWorkshop
+{
+    public class AnagramCheck
+    {
+        public static void CheckAnagram()
+        {
+            Console.WriteLine("Enter the first string:");
+            string str1 = Console.ReadLine();
+
+            Console.WriteLine("Enter the second string");
+            string str2 = Console.ReadLine();
+
+            char[] ch1 = str1.ToLower().ToCharArray();
+            char[] ch2 = str1.ToLower().ToCharArray();
+
+            Array.Sort(ch1);
+            Array.Sort(ch2);
+
+            string val1 = new string(ch1);
+            string val2 = new string(ch2);
+
+            if (val1==val2)
+            {
+                Console.WriteLine("Both the string are anagram");
+            }
+            else
+            {
+                Console.WriteLine("Both the string are not Anagram");
+            }
+        }
+    }
+}
---
+++ DayWorkshop/DayWorkshop/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DayWorkshop
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to the code of anngram");
+            AnagramCheck.CheckAnagram();
+        }
+    }
+}
